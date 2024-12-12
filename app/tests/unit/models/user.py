c56fb1c9d1c43067305a94271483e521531402d4
Taras <tsmereka@protonmail.com>
2024-12-12 15:28:11 -0600

    started on tests

--- app/tests/unit/models/user.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from app.models import User
-
-class TestUser(unittest.TestCase):
-    def test_create_user(self):
-        # test creating a new user
-        pass
--- dynamodb/services/tea_service.py
+++ dynamodb/services/tea_service.py
@@ -1,29 +1,19 @@
 """Services for teas"""
-from services.dynamodb_utils import create_transaction, commit_transaction, rollback_transaction
-import boto3
-
-dynamodb = boto3.resource('dynamodb')
-
 class TeaService:
-    def __init__(self):
-        self.table_name = 'tea_table'
+    def __init__(self, tea_table):
+        self.tea_table = tea_table
 
-    def get_tea(self, tea_id):
-        """Get tea from database"""
-        pass
+    def get_tea_item(self, name):
+        return self.tea_table.get_tea_item(name)
 
-    def create_tea(self, tea_item):
-        """Create a new tea in the database"""
-        transaction = create_transaction(self.table_name)
-        try:
-            # Create the tea item in the table
-            table = dynamodb.Table(self.table_name)
-            table.put_item(Item=tea_item)
-            commit_transaction(transaction)
-        except Exception as e:
-            rollback_transaction(transaction)
-            raise e
+    def create_tea_item(self, tea_item):
+        self.tea_table.create_tea_item(tea_item)
 
-    def update_tea(self, tea_id, tea_data):
-        """Update an existing tea in the database"""
-        pass
+    def update_tea_item(self, tea_item):
+        self.tea_table.update_tea_item(tea_item)
+
+    def increment_steep_count(self, name):
+        self.tea_table.increment_steep_count(name)
+
+    def delete_tea_item(self, name):
+        self.tea_table.delete_tea_item(name)
