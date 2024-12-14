6e5034bf828006eebe96f9b25e4cea4bb7371e6f
Taras <tsmereka@protonmail.com>
2024-12-13 21:07:18 -0600

    tea model

--- app/models/tea.py
+++ app/models/tea.py
@@ -1,10 +1,53 @@
-from dataclasses import dataclass
+"""Defines the Tea class"""
 
-@dataclass
 class Tea:
-    id: int
-    name: str
-    type: str
-    steep_time_seconds: int
-    steep_temperature_fahrenheit: int
-    steep_count: int
+    def __init__(self, name, tea_type, steep_time_seconds = 0, steep_temperature_fahrenheit = 0, steep_count = 0):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        if not type:
+            raise ValueError("Type cannot be empty")
+        self.name = name
+        self.tea_type = tea_type
+        self.steep_time_seconds = steep_time_seconds
+        self.steep_temperature_fahrenheit = steep_temperature_fahrenheit
+        self.steep_count = steep_count
+
+    def __eq__(self, other):
+        if not isinstance(other, Tea):
+            return False
+        return (
+            self.name == other.name and
+            self.tea_type == other.tea_type and
+            self.steep_time_seconds == other.steep_time_seconds and
+            self.steep_temperature_fahrenheit == other.steep_temperature_fahrenheit and
+            self.steep_count == other.steep_count
+        )
+
+    def __repr__(self):
+        return (f'Tea(name="{self.name}", type="{self.tea_type}", steep_time_seconds={self.steep_time_seconds},'
+                f'steep_temperature_fahrenheit={self.steep_temperature_fahrenheit}, steep_count={self.steep_count})')
+
+    def to_dict(self):
+        return {
+            'Name': self.name,
+            'Type': self.tea_type,
+            'SteepTimeSeconds': self.steep_time_seconds,
+            'SteepTemperatureFahrenheit': self.steep_temperature_fahrenheit,
+            'SteepCount': self.steep_count
+        }
+
+    @staticmethod
+    def from_dict(tea_dict):
+        if not tea_dict:
+            raise ValueError("Tea dictionary cannot be empty")
+        if not tea_dict.get("Name"):
+            raise ValueError("Name cannot be empty")
+        if not tea_dict.get("Type"):
+            raise ValueError("Type cannot be empty")
+        return Tea(
+            tea_dict.get('Name'),
+            tea_dict.get('Type'),
+            tea_dict.get('SteepTimeSeconds', 0),
+            tea_dict.get('SteepTemperatureFahrenheit', 0),
+            tea_dict.get('SteepCount', 0),
+        )
---
+++ tests/unit/models/test_tea.py
@@ -0,0 +1,79 @@
+import pytest
+from app.models.tea import Tea
+
+class TestTeaModel:
+    def test_init(self):
+        tea = Tea("Earl Grey", "Black")
+        assert tea.name == "Earl Grey"
+        assert tea.tea_type == "Black"
+        assert tea.steep_time_seconds == 0
+        assert tea.steep_temperature_fahrenheit == 0
+        assert tea.steep_count == 0
+
+    def test_init_with_steep_time(self):
+        tea = Tea("Earl Grey", "Black", steep_time_seconds=3)
+        assert tea.name == "Earl Grey"
+        assert tea.tea_type == "Black"
+        assert tea.steep_time_seconds == 3
+        assert tea.steep_temperature_fahrenheit == 0
+        assert tea.steep_count == 0
+
+    def test_init_with_steep_temperature(self):
+        tea = Tea("Earl Grey", "Black", steep_temperature_fahrenheit=200)
+        assert tea.name == "Earl Grey"
+        assert tea.tea_type == "Black"
+        assert tea.steep_time_seconds == 0
+        assert tea.steep_temperature_fahrenheit == 200
+        assert tea.steep_count == 0
+
+    def test_init_with_steep_count(self):
+        tea = Tea("Earl Grey", "Black", steep_count=2)
+        assert tea.name == "Earl Grey"
+        assert tea.tea_type == "Black"
+        assert tea.steep_time_seconds == 0
+        assert tea.steep_temperature_fahrenheit == 0
+        assert tea.steep_count == 2
+
+    def test_repr(self):
+        tea = Tea("Earl Grey", "Black")
+        assert repr(tea) == ('Tea(name="Earl Grey", type="Black", steep_time_seconds=0,'
+                             'steep_temperature_fahrenheit=0, steep_count=0)')
+
+    def test_eq(self):
+        tea1 = Tea("Earl Grey", "Black")
+        tea2 = Tea("Earl Grey", "Black")
+        assert tea1 == tea2
+
+    def test_to_dict(self):
+        tea = Tea("Earl Grey", "Black")
+        expected_dict = {
+            "Name": "Earl Grey",
+            "Type": "Black",
+            "SteepTimeSeconds": 0,
+            "SteepTemperatureFahrenheit": 0,
+            "SteepCount": 0
+        }
+        assert tea.to_dict() == expected_dict
+
+    def test_from_dict(self):
+        tea_dict = {
+            "Name": "Earl Grey",
+            "Type": "Black",
+            "SteepTimeSeconds": 3,
+            "SteepTemperatureFahrenheit": 200,
+            "SteepCount": 2
+        }
+        tea = Tea.from_dict(tea_dict)
+        assert tea.name == "Earl Grey"
+        assert tea.type == "Black"
+        assert tea.steep_time_seconds == 3
+        assert tea.steep_temperature_fahrenheit == 200
+        assert tea.steep_count == 2
+
+    def test_from_dict_invalid_name(self):
+        with pytest.raises(ValueError):
+            Tea.from_dict({"Type": "Black"})
+
+    def test_from_dict_invalid_type(self):
+        with pytest.raises(ValueError):
+            Tea.from_dict({"Name": "Earl Grey"})
