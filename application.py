31f5e8dedd853fb0edbfe0ba3567e7c1a33875fa
Taras <tsmereka@protonmail.com>
2025-01-03 16:31:23 -0600

    working through logging on EB

--- application.py
+++ application.py
@@ -1,4 +1,12 @@
+import logging
 from app.app import create_app
+
+# Set up logging
+logging.basicConfig(
+    filename='/var/log/flask/application.log',
+    level=logging.INFO,
+    format='%(asctime)s %(levelname)s: %(message)s'
+)
 
 application = create_app()
 
